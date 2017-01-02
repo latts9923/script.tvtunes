72f72b92a264afaf5bedfed855b3c7806cc04d22
robwebset <robwebset@users.noreply.github.com>
2017-01-02 14:35:30 +0000

    Add support for webm video files and Trailers

--- service.py
+++ service.py
@@ -61,37 +61,28 @@ if __name__ == '__main__':
         # else:
         #    log("TvTunesService: Uploader not enabled")
 
-        json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Addons.GetAddonDetails", "params": { "addonid": "repository.robwebset", "properties": ["enabled", "broken", "name", "author"]  }, "id": 1}')
+        displayNotice = True
+        json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Addons.GetAddonDetails", "params": { "addonid": "repository.urepo", "properties": ["enabled", "broken", "name", "author"]  }, "id": 1}')
         json_response = json.loads(json_query)
 
-        displayNotice = True
         if ("result" in json_response) and ('addon' in json_response['result']):
             addonItem = json_response['result']['addon']
-            if (addonItem['enabled'] is True) and (addonItem['broken'] is False) and (addonItem['type'] == 'xbmc.addon.repository') and (addonItem['addonid'] == 'repository.robwebset') and (addonItem['author'] == 'robwebset'):
+            if (addonItem['enabled'] is True) and (addonItem['broken'] is False) and (addonItem['type'] == 'xbmc.addon.repository') and (addonItem['addonid'] == 'repository.urepo'):
                 displayNotice = False
 
+                # Create a monitor so we can reload the settings if they change
+                systemMonitor = TvTunesMonitor()
+
+                # Start looping to perform the TvTune theme operations
+                main = TunesBackend()
+
+                # Start the themes running
+                main.runAsAService()
+
+                del main
+                del systemMonitor
+
         if displayNotice:
-            json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Addons.GetAddonDetails", "params": { "addonid": "repository.urepo", "properties": ["enabled", "broken", "name", "author"]  }, "id": 1}')
-            json_response = json.loads(json_query)
-
-            if ("result" in json_response) and ('addon' in json_response['result']):
-                addonItem = json_response['result']['addon']
-                if (addonItem['enabled'] is True) and (addonItem['broken'] is False) and (addonItem['type'] == 'xbmc.addon.repository') and (addonItem['addonid'] == 'repository.urepo'):
-                    displayNotice = False
-
-        if displayNotice:
-            xbmc.executebuiltin('Notification("robwebset or URepo Repository Required","github.com/robwebset/repository.robwebset",10000,%s)' % ADDON.getAddonInfo('icon'))
-        else:
-            # Create a monitor so we can reload the settings if they change
-            systemMonitor = TvTunesMonitor()
-
-            # Start looping to perform the TvTune theme operations
-            main = TunesBackend()
-
-            # Start the themes running
-            main.runAsAService()
-
-            del main
-            del systemMonitor
+            xbmc.executebuiltin('Notification("URepo Repository Required","www.urepo.org",10000,%s)' % ADDON.getAddonInfo('icon'))
     else:
         log("TvTunesService: Theme playing disabled")
